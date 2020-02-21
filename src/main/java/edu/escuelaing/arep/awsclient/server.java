7cdc2742c20ce4c30c09ecb50b302429b38c7d7e
unknown <santiago.vega-r@labinfo.is.escuelaing.edu.co>
2020-02-21 12:55:39 -0500

    Ni idea

---
+++ src/main/java/edu/escuelaing/arep/awsclient/server.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.escuelaing.arep.awsclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+
+/**
+ *
+ * @author santiago.vega-r
+ */
+public class server {
+     public static void main(String[] args) throws Exception { 
+      Boolean running = true;
+      while (running){
+          ClientSocket cc = new 
+      }
+    } 
+}
